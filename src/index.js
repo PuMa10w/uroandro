b2572ab174bf602011008c701b444238f8f4e72a
rousl <rousl@example.com>
2026-06-15 21:01:06 +0300

    feat: add design system token & typography CSS and import them

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './design/tokens.css';
+import './design/typography.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { initSentry } from './utils/sentry';
